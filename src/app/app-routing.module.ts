adfbeaa4bad8982f7eb10771f9f1c651e33466c8
Zeljko Djuric <zeljko94@gmail.com>
2020-02-01 16:50:56 +0100

    Added component About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
-const routes: Routes = [{ path: "dashboard", component: DashboardComponent }];
+import { AboutComponent } from "./components/about/about.component";
+
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
