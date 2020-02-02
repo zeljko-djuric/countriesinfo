0b58e68a7b14c4a8b95eba89d0c4051ce771c7a9
Zeljko Djuric <zeljko94@gmail.com>
2020-02-02 15:32:49 +0100

    Added Angular service for api, setup for HttpClient

---
+++ src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  public getNews() {
+    return this.httpClient.get(`https://restcountries.eu/rest/v2/all`);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AboutComponent } from './components/about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,11 +15,8 @@ import { AboutComponent } from './components/about/about.component';
     DashboardComponent,
     AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
